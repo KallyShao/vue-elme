a9ca4acc383759b14325a9df4d7f5c4d6da3daa7
Kally Shao <188963758@qq.com>
2018-10-15 17:56:46 +0800

    商品列表左右菜单联动处理未完成

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,23 +1,50 @@
 <template>
-    <div class="wrapper">
-        <div class="left-wrapper">
+    <div class="outer-wrapper">
+        <div class="left-wrapper" ref="leftWrapper">
             <ul class="menu-list">
-                <li class="menu-item" v-for="(item, index) in goods" :key="index">
-                    <span class="text">
+                <li class="menu-item" v-for="(item, index) in goods" :key="index" :class="{'active': index == getCurrentMenuIndex}">
+                    <span class="text border-1px">
                         <span v-show="item.type >= 0" class="icon">
-                            <v-icon :size="32" :type="item.type" :voidFlag="1"></v-icon>
+                            <v-icon :size="24" :type="item.type" :voidFlag="1"></v-icon>
                         </span>
                         {{ item.name }}
                     </span>
                 </li>
             </ul>
         </div>
-        <div class="foods-wrapper"></div>
+        <div class="foods-wrapper" ref="foodsWrapper">
+            <div>
+                <div v-for="(goodsItem, index) in goods" class="category-item category-item" :key="index">
+                    <h1 class="category-item-title">{{ goodsItem.name }}</h1>
+                    <ul class="foods-list">
+                        <li class="foods-item border-1px" v-for="(foodsItem, index) in goodsItem.foods" :key="index">
+                            <div class="img-wrap">
+                                <img :src="foodsItem.icon">
+                            </div>
+                            <div class="text-wrap">
+                                <h2 class="text-title">{{ foodsItem.name }}</h2>
+                                <p class="description">{{ foodsItem.description }}</p>
+                                <p class="rating-wrap">
+                                    <span class="sell-count">月售{{ foodsItem.sellCount }}份</span><span class="rating">好评率{{ foodsItem.rating }}%</span>
+                                </p>
+                                <div class="price">
+                                    <span class="current-price"><span class="money-flag">￥</span>{{ foodsItem.price }}</span>
+                                    <s class="old-price" v-if="foodsItem.oldPrice">￥{{ foodsItem.oldPrice }}</s>
+                                </div>
+                                <i class="icon-add_circle"></i>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import icon from 'components/common/icon/icon';
+import BScroll from 'better-scroll';
+
 const ERR_OK = 0;
 export default {
     props: {
@@ -27,18 +54,59 @@ export default {
     },
     data() {
         return {
-            goods: []
+            goods: [],
+            heightList: [],
+            scrollY: 0
         };
     },
     components: {
         'v-icon': icon
     },
+    computed: {
+        getCurrentMenuIndex() {
+            // let index = 0;
+            for (let i = 0; i < this.heightList.length; i++){
+                if (this.scrollY < this.heightList[i + 1] && this.scrollY >= this.heightList[i]){
+                    console.log(i);
+                    return i;
+                }
+            }
+        }
+    },
+    methods: {
+        _initScroll() {
+            let leftWrapper = this.$refs.leftWrapper;
+            this.leftScroll = new BScroll(leftWrapper);
+
+            let foodsWrapper = this.$refs.foodsWrapper;
+            this.foodsScroll = new BScroll(foodsWrapper, {
+                probeType: 3
+            });
+        },
+        _calculateHeight() {
+            let height = 0;
+            let foodsWrapper = this.$refs.foodsWrapper;
+            let categoryItemList = foodsWrapper.getElementsByClassName('category-item-hook');
+            for (let i = 0; i < categoryItemList.length; i++){
+                height += categoryItemList[i].offsetHeight;
+                this.heightList.push(height);
+            }
+        }
+    },
     created() {
         this.$http.get('/api/goods').then((res) => {
-            console.log(res);
             res = res.body;
             if (res.errno === ERR_OK){
                 this.goods = res.data;
+                // 从接口中取到的数据是异步更新的，而dom的更新是在nextTick()中进行
+                this.$nextTick(() => {
+                    this._initScroll();
+                    this._calculateHeight();
+                    this.foodsScroll.on('scroll', (pos) => {
+                        this.scrollY = Math.abs(Math.round(pos.y));
+                        // console.log(this.scrollY);
+                    });
+                });
             }
         });
     }
@@ -46,7 +114,8 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-.wrapper
+@import '~stylus/mixin.styl';
+.outer-wrapper
     display: flex
     position: absolute
     top: 174px
@@ -59,18 +128,90 @@ export default {
         background: #f3f5f7
         .menu-list
             padding: 0 12px
+            overflow: hidden
             .menu-item
+                display: table
                 width: 56px
                 height: 54px
                 line-height: 14px
-                font-size: 12px
                 color: #666
+                &.active
+                    background: #fff
                 .text
+                    display: table-cell
+                    vertical-align: middle
                     line-height: 14px
-                    // vertical-align: middle
-                    .icon
-                        display: inline-block
-                        vertical-align: bottom
+                    border-1px(rgba(7, 17, 27, .1))
+                    font-size: 12px
+                .icon
+                    display: inline-block
+                    margin-right: 1px
+                    vertical-align: top
     .foods-wrapper
         flex: 1
+        .category-item-title
+            height: 26px
+            line-height: 26px
+            padding-left: 14px
+            font-size: 12px
+            color: rgb(147, 153, 159)
+            background: #f3f5f7
+            border-left: 2px solid #d9dde1
+        .foods-list
+            .foods-item
+                display: flex
+                position: relative
+                margin: 18px
+                padding-bottom: 18px
+                font-size: 0
+                border-1px(rgba(7, 17, 27, .1))
+                &:last-child
+                    margin-bottom: 0
+                    border-none()
+                .icon-add_circle
+                    position: absolute
+                    right: 0
+                    bottom: 18px
+                    line-height: 24px
+                    font-size: 24px
+                    color: rgb(0, 160, 220)
+                .img-wrap
+                    display: inline-block
+                    flex: 0 0 57px
+                    img
+                        width: 57px
+                        height: 57px
+                .text-wrap
+                    flex: 1
+                    display: inline-block
+                    vertical-align: top
+                    padding-left: 10px
+                    .text-title
+                        line-height: 14px
+                        margin-top: 2px
+                        font-size: 14px
+                        color: rgb(7, 17, 27)
+                    .description, .rating-wrap
+                        line-height: 12px
+                        margin-top: 8px
+                        font-size: 10px
+                        color: rgb(147, 153, 159)
+                    .rating-wrap
+                        .sell-count
+                            margin-right: 12px
+                    .price
+                        height: 24px
+                        line-height: 24px
+                        .current-price
+                            margin-right: 8px
+                            color: #f01414
+                            font-size: 14px
+                            font-weight: 700
+                            .money-flag
+                                font-size: 10px
+                                font-weight: normal
+                        .old-price
+                            font-weight: 700
+                            font-size: 10px
+                            color: #93999f
 </style>
